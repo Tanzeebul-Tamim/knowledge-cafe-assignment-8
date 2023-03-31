db5087720228b48645a76400937b85178eaf4e5a
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-04-01 03:28:30 +0600

    data loaded

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Body.css';
 
 const Body = () => {
+    const [postCards, setPostCards] = useState ([]);
+    
+    useEffect (() => {
+        fetch ("data.json")
+        .then (res => res.json())
+        .then (data => setPostCards(data));
+    }, []);
+
     return (
         <div>
-            <div>
-                <img src="" alt="" />
+            <div className='posts-container'>
+                {
+                    postCards.map(postCard => console.log(postCard))
+                }
             </div>
             <div>
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-       <div className='header'>
-         <div className='d-flex align-items-center justify-content-between px-4 pt-4'>
-            <div>
-                <h1 className='h2 fw-bold'>Dev Diaries</h1>
-            </div>
-            <div className='nav d-flex align-items-center'>
-                <a href="/profile">Profile</a>
-                <a href="/login">Login</a>
-                <a href="/contact">Contact</a>
-                <a href="/about">About</a>
-                <img className='img-fluid rounded-circle' src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
-            </div>
+  return (
+    <div className="header">
+      <div className="d-flex align-items-center justify-content-between px-4 py-2">
+        <div>
+          <h1 className="h2 fw-bold">Dev Diaries</h1>
         </div>
-        <hr />
-       </div>
-    );
+        <div className="nav d-flex align-items-center">
+          <div className="d-none d-sm-block">
+            <a href="/profile">Profile</a>
+            <a href="/login">Login</a>
+            <a href="/contact">Contact</a>
+            <a href="/about">About</a>
+          </div>
+          <div>
+            <img
+              className="img-fluid rounded-circle"
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

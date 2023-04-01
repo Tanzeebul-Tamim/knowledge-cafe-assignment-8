215b580a22bf88054c74927db88b1317c89096a3
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-04-01 17:37:53 +0600

    bookmark icon toggled

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Body from './components/Body/Body';
-import Header from './components/Header/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Body from "./components/Body/Body";
+import Header from "./components/Header/Header";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
-     <Body></Body>
+      <Header></Header>
+      <Body></Body>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,45 +1,115 @@
-import React from "react";
-import './PostCard.css';
+import React, { useState } from "react";
+import "./PostCard.css";
+import { ToastContainer, toast } from "react-toastify";
 
-const PostCard = ({cardData, handleReadTime}) => { 
+const PostCard = ({ cardData, handleReadTime }) => {
 
-  const handleBookmark = id => {
+  const handleBookmark = (id) => {
+
     const previousBookmark = JSON.parse(localStorage.getItem("bookmark"));
     let bookmark = [];
-    const postCard = {id};
+    const postCard = { id };
 
+    if (previousBookmark) {
+      const alreadyBookmarked = previousBookmark.find((card) => card.id === id);
+      if (alreadyBookmarked) {
+        toast.warn("This post is already bookmarked!!", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      else {
+        bookmark.push(...previousBookmark, postCard);
+        localStorage.setItem("bookmark", JSON.stringify(bookmark));
+        toast.success("This post has been bookmarked successfully.", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    }
+    else {
+      bookmark.push(postCard);
+      localStorage.setItem("bookmark", JSON.stringify(bookmark));
+      toast.success("This post has been bookmarked successfully.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+  const [bookmarked, setBookmarked] = useState(false);
+
+  const checkBookmark = id => {
+    const previousBookmark = JSON.parse(localStorage.getItem("bookmark"));
+    const alreadyBookmarked = previousBookmark.find((card) => card.id === id);
+    if (alreadyBookmarked) {
+      setBookmarked (true);
+    }
+    else {
+      setBookmarked (false);
+    }
   }
 
   return (
     <div>
-      <img className="rounded-4 cover img-fluid w-100" src={cardData.cover_image}/>
+      <img
+        className="rounded-4 cover img-fluid w-100"
+        src={cardData.cover_image}
+      />
       <div className="d-flex justify-content-between align-items-center mt-4">
         <div className="d-flex align-items-center gap-3">
-          <img className="rounded-circle author-image" src={cardData.author_image}/>
+          <img
+            className="rounded-circle author-image"
+            src={cardData.author_image}
+          />
           <div className="name-date">
             <p className="h5 fw-bold">{cardData.author_name}</p>
-            <p className="h6 date">{cardData.post_date} ({cardData.days_ago} days ago)</p>
+            <p className="h6 date">
+              {cardData.post_date} ({cardData.days_ago} days ago)
+            </p>
           </div>
         </div>
         <div className="d-flex align-items-center gap-2">
-          <p className="h5 read-time">
-            {cardData.read_time} min read
-          </p>
-          <button className="h5 mark" href=""><i onClick={() => handleBookmark (cardData.id)} title="Bookmark this post" class="fa-regular fa-bookmark"></i></button>
+          <p className="h5 read-time">{cardData.read_time} min read</p>
+          <button className="h5 mark" href="">
+            <i
+              onClick={() => {handleBookmark(cardData.id); checkBookmark(cardData.id)}}
+              title="Bookmark this post"
+              class={bookmarked ? "fa-solid fa-bookmark" : "fa-regular fa-bookmark"}
+            ></i>
+          </button>
         </div>
       </div>
       <div className="mt-3">
-        <h1 className="fw-bold h1">
-          {cardData.title}
-        </h1>
+        <h1 className="fw-bold h1">{cardData.title}</h1>
       </div>
-      <p className="h5 my-3 hashtag">
-        {cardData.hashtags}
-      </p>
-      <button onClick={() => handleReadTime (cardData.read_time)} className="h5 fw-bold mark" href="">
+      <p className="h5 my-3 hashtag">{cardData.hashtags}</p>
+      <button
+        onClick={() => handleReadTime(cardData.read_time)}
+        className="h5 fw-bold" id="mark"
+        href=""
+      >
         Mark as read
       </button>
-      <hr className="mb-5"/>
+      <hr className="mb-5" />
     </div>
   );
 };

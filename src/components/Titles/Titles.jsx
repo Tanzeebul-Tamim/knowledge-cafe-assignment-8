f2db7661362d64cc97f14a68d265cddd478d8c8c
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-04-01 22:47:38 +0600

    done

--- src/components/Titles/Titles.jsx
+++ src/components/Titles/Titles.jsx
@@ -4,7 +4,7 @@ import './Titles.css';
 const Titles = ({ar}) => {
     return (
         <div className='mb-3 ms-3 title-container'>
-            <h1 className='h5'>{ar}</h1>
+            <h1 className='h6 fw-bold'>{ar}</h1>
         </div>
     );
 };
